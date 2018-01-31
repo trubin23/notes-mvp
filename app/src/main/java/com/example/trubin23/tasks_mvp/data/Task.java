d505b59f4f1537e8007631e98a642ed73835c355
Andrey Trubin <trubin23@gmail.com>
2018-01-31 16:05:05 +0500

    created class TaskData with Room

--- app/src/main/java/com/example/trubin23/tasks_mvp/data/Task.java
+++ app/src/main/java/com/example/trubin23/tasks_mvp/data/Task.java
@@ -1,12 +1,38 @@
 package com.example.trubin23.tasks_mvp.data;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
 /**
  * Created by Andrey on 30.01.2018.
  */
 
-public class Task {
+@Entity(tableName = "tasks")
+public final class Task {
 
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    private final String mId;
+
+    @NonNull
+    @ColumnInfo(name = "title")
     private String mTitle;
+
+    @NonNull
+    @ColumnInfo(name = "description")
     private String mDescription;
-    private String mDate;
+
+    @NonNull
+    @ColumnInfo(name = "date-of-creation")
+    private String mDateOfCreation;
+
+    public Task(String id, String title, String description, String dateOfCreation) {
+        mId = id;
+        mTitle = title;
+        mDescription = description;
+        mDateOfCreation = dateOfCreation;
+    }
 }
---
+++ app/src/main/java/com/example/trubin23/tasks_mvp/data/source/local/TasksDao.java
@@ -0,0 +1,32 @@
+package com.example.trubin23.tasks_mvp.data.source.local;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.trubin23.tasks_mvp.data.Task;
+
+import java.util.List;
+
+/**
+ * Created by Andrey on 31.01.2018.
+ */
+
+@Dao
+public interface TasksDao {
+
+    @Query("SELECT * FROM tasks")
+    List<Task> getTasks();
+
+    @Query("SELECT * FROM tasks WHERE id = :id")
+    Task getTaskById(String id);
+
+    @Update
+    int updateTask(Task task);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertTask(Task task);
+
+}
