e56759c6a9fc94633ac061b5153a1a7909eb14e3
Andrey Trubin <trubin23@gmail.com>
2018-02-01 18:09:07 +0500

    implementation class of Injection

--- app/src/main/java/com/example/trubin23/tasks_mvp/data/source/TasksRepository.java
+++ app/src/main/java/com/example/trubin23/tasks_mvp/data/source/TasksRepository.java
@@ -9,6 +9,22 @@ import com.example.trubin23.tasks_mvp.data.Task;
  */
 
 public class TasksRepository implements TasksDataSource {
+
+    private static TasksRepository INSTANCE;
+
+    private final TasksDataSource mTasksLocalDataSource;
+
+    private TasksRepository(@NonNull TasksDataSource tasksLocalDataSource) {
+        mTasksLocalDataSource = tasksLocalDataSource;
+    }
+
+    public static TasksRepository getInstance(@NonNull TasksDataSource tasksLocalDataSource) {
+        if (INSTANCE == null) {
+            INSTANCE = new TasksRepository(tasksLocalDataSource);
+        }
+        return INSTANCE;
+    }
+
     @Override
     public void getTasks(@NonNull LoadTasksCallback callback) {
 
--- app/src/main/java/com/example/trubin23/tasks_mvp/util/Injection.java
+++ app/src/main/java/com/example/trubin23/tasks_mvp/util/Injection.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 
 import com.example.trubin23.tasks_mvp.data.source.TasksRepository;
+import com.example.trubin23.tasks_mvp.data.source.local.TasksDatabase;
+import com.example.trubin23.tasks_mvp.data.source.local.TasksLocalDataSource;
 
 /**
  * Created by Andrey on 01.02.2018.
@@ -13,6 +15,9 @@ public class Injection {
 
     @Nullable
     public static TasksRepository provideTasksRepository(Context context) {
-        return null;
+        TasksDatabase database = TasksDatabase.getInstance(context);
+
+        return TasksRepository.getInstance(
+                TasksLocalDataSource.getInstance(new AppExecutors(), database.taskDao()));
     }
 }
