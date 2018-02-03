223c834f66e23436be11e7ac5c1152a53af2f5b1
Andrey Trubin <trubin23@gmail.com>
2018-02-03 20:46:06 +0500

    preparing to adding TaskDetailActivity

---
+++ app/src/main/java/com/example/trubin23/tasks_mvp/taskdetail/TaskDetailActivity.java
@@ -0,0 +1,20 @@
+package com.example.trubin23.tasks_mvp.taskdetail;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.trubin23.tasks_mvp.R;
+
+/**
+ * Created by Andrey on 03.02.2018.
+ */
+
+public class TaskDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.taskdetail_act);
+    }
+}
