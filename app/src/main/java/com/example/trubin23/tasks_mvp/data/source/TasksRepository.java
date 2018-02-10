27c984d611f556ea83cd71877d24d1ff74fb1c64
Andrey Trubin <trubin23@gmail.com>
2018-02-10 17:02:59 +0500

    process adding CacheTasks in TasksRepository

--- app/src/main/java/com/example/trubin23/tasks_mvp/data/source/TasksRepository.java
+++ app/src/main/java/com/example/trubin23/tasks_mvp/data/source/TasksRepository.java
@@ -5,7 +5,10 @@ import android.support.annotation.NonNull;
 import com.example.trubin23.tasks_mvp.data.Task;
 import com.example.trubin23.tasks_mvp.util.AppExecutors;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Andrey on 31.01.2018.
@@ -18,6 +21,10 @@ public class TasksRepository implements TasksDataSource {
     private AppExecutors mAppExecutors;
 
     private final TasksDataSource mTasksLocalDataSource;
+
+    private Map<String, Task> mCachedTasks;
+
+    private boolean mCacheIsDirty = false;
 
     private TasksRepository(@NonNull AppExecutors appExecutors,
                             @NonNull TasksDataSource tasksLocalDataSource) {
@@ -36,19 +43,43 @@ public class TasksRepository implements TasksDataSource {
 
     @Override
     public void getTasks(@NonNull final LoadTasksCallback callback) {
-        Runnable runnable = () -> mTasksLocalDataSource.getTasks(new LoadTasksCallback() {
-            @Override
-            public void onTasksLoaded(final List<Task> tasks) {
-                mAppExecutors.getMainThread().execute(() -> callback.onTasksLoaded(tasks));
-            }
+        if (mCachedTasks != null && !mCacheIsDirty) {
+            callback.onTasksLoaded(new ArrayList<>(mCachedTasks.values()));
+            return;
+        }
 
-            @Override
-            public void onDataNotAvailable() {
-                mAppExecutors.getMainThread().execute(callback::onDataNotAvailable);
-            }
-        });
+        if (mCacheIsDirty) {
+            getTasksFromRemoteDateSource(callback);
+        } else {
+            Runnable runnable = () -> mTasksLocalDataSource.getTasks(new LoadTasksCallback() {
+                @Override
+                public void onTasksLoaded(final List<Task> tasks) {
+                    refreshCache(tasks);
+                    mAppExecutors.getMainThread().execute(() -> callback.onTasksLoaded(tasks));
+                }
 
-        mAppExecutors.getDiskIO().execute(runnable);
+                @Override
+                public void onDataNotAvailable() {
+                    getTasksFromRemoteDateSource(callback);
+                }
+            });
+
+            mAppExecutors.getDiskIO().execute(runnable);
+        }
+    }
+
+    private void refreshCache(List<Task> tasks) {
+        if (mCachedTasks == null) {
+            mCachedTasks = new LinkedHashMap<>();
+        }
+        mCachedTasks.clear();
+        for (Task task : tasks) {
+            mCachedTasks.put(task.getId(), task);
+        }
+        mCacheIsDirty = false;
+    }
+
+    private void getTasksFromRemoteDateSource(@NonNull LoadTasksCallback callback) {
     }
 
     @Override
