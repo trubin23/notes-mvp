b266319a7a8c91941c4244c7d52a225510b672b5
Andrey Trubin <trubin23@gmail.com>
2018-02-17 12:18:48 +0500

    adding NavigationView to StatisticsActivity

--- app/src/main/java/com/example/trubin23/tasks_mvp/statistics/StatisticsActivity.java
+++ app/src/main/java/com/example/trubin23/tasks_mvp/statistics/StatisticsActivity.java
@@ -1,5 +1,9 @@
 package com.example.trubin23.tasks_mvp.statistics;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.NavUtils;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,10 +13,19 @@ import com.example.trubin23.tasks_mvp.util.Injection;
 
 public class StatisticsActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics_act);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
 
         StatisticsFragment statisticsFragment =
                 (StatisticsFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
@@ -24,5 +37,24 @@ public class StatisticsActivity extends AppCompatActivity {
 
         new StatisticsPresenter(Injection.provideTasksRepository(getApplicationContext()),
                 statisticsFragment);
+    }
+
+    private void setupDrawerContent(@NonNull NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                item -> {
+                    switch (item.getItemId()) {
+                        case R.id.list_nav_menu_item:
+                            NavUtils.navigateUpFromSameTask(StatisticsActivity.this);
+                            break;
+                        case R.id.statistics_nav_menu_item:
+                            break;
+                        default:
+                            break;
+                    }
+
+                    mDrawerLayout.closeDrawers();
+                    return true;
+                }
+        );
     }
 }
